79288e884e92203c8ffa7013c673c15957f9df84
krm_shrrftdnv <Karim.Sharafutdinov@easycomm.ru>
2023-07-18 17:15:52 +0300

    rollback changes with duration flag

--- internal/config.go
+++ internal/config.go
@@ -1,9 +1,7 @@
 package internal
 
-import "time"
-
 type Config struct {
-	ServerAddress  string        `env:"ADDRESS"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
+	ServerAddress  string `env:"ADDRESS"`
+	PollInterval   int64  `env:"POLL_INTERVAL"`
+	ReportInterval int64  `env:"REPORT_INTERVAL"`
 }
