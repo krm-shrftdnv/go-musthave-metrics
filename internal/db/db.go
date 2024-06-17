7c8e548d7fdaf28303d767d09de3a84e0e168bcc
krm_shrrftdnv <Karim.Sharafutdinov@easycomm.ru>
2024-06-18 01:14:56 +0300

    ping db after init

--- internal/db/db.go
+++ internal/db/db.go
@@ -31,6 +31,10 @@ func Init(db *sql.DB, databaseDsn string) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
 	return db, nil
 }
 
